bd926bee64b743b0322ef0a79f83c30e03b2e6bb
Vladimir Aleksic <vlada.pzs@hotmail.com>
2022-04-20 17:01:31 +0200

    UI/UX app

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NTFCard from "./NTFCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NTFCard,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo
+}
